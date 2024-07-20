6cbb4914c381f6e4752cd914fc24c9b299df27d2
PedRaM <p.mohammadalipour@dotin.ir>
2024-07-20 18:19:32 +0330

    fix issue

--- lib/model/cow_id.dart
+++ lib/model/cow_id.dart
@@ -11,8 +11,8 @@ class CowId {
 
   String? get serialize {
     if (id == null && refId == null) return null;
-    if (id == null) return refId;
-    if (refId == null) return id;
+    if (id == null) return 'NULL:$refId';
+    if (refId == null) return '$id:NULL';
     return '$id:$refId';
   }
 }
